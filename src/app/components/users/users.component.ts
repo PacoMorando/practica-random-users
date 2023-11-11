d7f76e5bf121095133af56c66cea631e5a5b213f
NYP_PC <paco.morando@gmail.com>
2023-11-11 17:22:44 -0600

    Ances en tabla

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,18 +8,33 @@ import { User } from 'src/app/services/users-results';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
-  protected stringForTest: string = ''
   protected users: User[] = [];
+  protected tableClass: string = 'table table-hover';
 
-  constructor(private randomUserService: RandomUsersService){
-     // EJEMPLO DE COMO ESPERAR AL SERVICE PARA LEER TODO EL JSON
+  constructor(private randomUserService: RandomUsersService) {
+    // EJEMPLO DE COMO ESPERAR AL SERVICE PARA LEER TODO EL JSON
     // this.randomUserService.getUsersAsString().subscribe(data => {
     //     this.usersAsString = JSON.stringify(data);
     // });
     this.randomUserService.getUsers().subscribe(data => {
       console.log(data.results);
       this.users = data.results;
-      this.stringForTest = this.users[0].email;
+    });
+  }
+
+  protected switchTableClass() {
+    if (this.tableClass == 'table table-hover') {
+      this.tableClass = 'table table-hover table-striped';
+    } else {
+      this.tableClass = 'table table-hover';
+    }
+  }
+
+  protected sortByCountry(){
+    this.users.sort((a, b) => {
+      if (a.location.country < b.location.country) return -1;
+      if (a.location.country > b.location.country) return 1;
+      return 0;
     });
   }
 
